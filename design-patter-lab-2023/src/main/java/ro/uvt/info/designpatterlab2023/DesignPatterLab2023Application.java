e36dfe3fe717bbbbc15d8bdd7141d13bc4a29b12
LAPTOP-8KT0HOB2\fflor <fflorescu87@gmail.com>
2023-10-24 08:21:09 +0300

    created project

---
+++ design-patter-lab-2023/src/main/java/ro/uvt/info/designpatterlab2023/DesignPatterLab2023Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatterlab2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatterLab2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatterLab2023Application.class, args);
+    }
+
+}
---
+++ design-patter-lab-2023/src/test/java/ro/uvt/info/designpatterlab2023/DesignPatterLab2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatterlab2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatterLab2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
