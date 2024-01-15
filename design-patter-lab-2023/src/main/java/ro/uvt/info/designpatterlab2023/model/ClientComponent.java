bea81e1bc38a7ae616ce5157058b60e8d282034e
LAPTOP-8KT0HOB2\fflor <fflorescu87@gmail.com>
2024-01-15 21:36:24 +0200

    lab 6

---
+++ design-patter-lab-2023/src/main/java/ro/uvt/info/designpatterlab2023/model/ClientComponent.java
@@ -0,0 +1,31 @@
+package ro.uvt.info.designpatterlab2023.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ClientComponent {
+    private TransientComponent tc;
+    private SingletonComponent sc;
+    public ClientComponent() {
+        System.out.println("ClientComponent::ClientComponent = " + this);
+    }
+
+    @Autowired
+    public void setTransientComponent(TransientComponent tc) {
+        this.tc = tc;
+    }
+
+    @Autowired
+    public void setSingletonComponent(SingletonComponent sc) {
+        this.sc = sc;
+    }
+
+    public void operation() {
+        System.out.println("Invoked ClientComponent::operation() on " + this);
+        System.out.println(" o SingletonComponent = " + sc);
+        System.out.println(" o TransientComponent = " + tc);
+    }
+}
---
+++ design-patter-lab-2023/src/main/java/ro/uvt/info/designpatterlab2023/model/Services/Controllers/HelloController.java
@@ -0,0 +1,32 @@
+package ro.uvt.info.designpatterlab2023.model.Services.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ro.uvt.info.designpatterlab2023.model.ClientComponent;
+import ro.uvt.info.designpatterlab2023.model.TransientComponent;
+import ro.uvt.info.designpatterlab2023.model.SingletonComponent;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class HelloController {
+    private final ClientComponent component;
+    private final ApplicationContext applicationContext;
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot";
+    }
+
+    @GetMapping("/client")
+    public String helloClient() {
+        TransientComponent transientComponent = applicationContext.getBean(TransientComponent.class);
+        SingletonComponent singletonComponent = applicationContext.getBean(SingletonComponent.class);
+
+        return "Hello from " + component.toString();
+    }
+}
